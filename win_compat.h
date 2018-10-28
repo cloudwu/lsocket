ca6e1d879f2d2869e9ec8688eacd2a619a93105b
Cloud Wu <cloudwu@gmail.com>
2018-10-28 17:55:45 +0800

    win compat improved

--- win_compat.h
+++ win_compat.h
@@ -1,57 +1,247 @@
-#ifndef WINDOWS_COMPAT_H
-#define WINDOWS_COMPAT_H
+#define WIN_COMPAT_IMPL
+#include "win_compat.h"
+#include "Iphlpapi.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
-#include <winsock2.h>
-#include <windows.h>
-#if _WIN32_WINNT < 0x0502
-#undef _WIN32_WINNT
-#define _WIN32_WINNT 0x0502
-#endif
+int 
+win_getsockopt(SOCKET sockfd, int level, int optname, void *optval, socklen_t *optlen) {
+	if (optname == SO_NOSIGPIPE) {
+		// ignore
+		return 0;
+	}
+	int size = (int)*optlen;
+	int ret = getsockopt(sockfd, level, optname, (char *)optval, &size);
+	if (ret == 0) {
+		*optlen = size;
+		return 0;
+	} else {
+		return -1;
+	}
+}
 
+int 
+win_setsockopt(SOCKET sockfd, int level, int optname, const void *optval, socklen_t optlen) {
+	if (optname == SO_NOSIGPIPE) {
+		// ignore
+		return 0;
+	}
+	int ret = setsockopt(sockfd, level, optname, (const char *)optval, (int)optlen);
+	if (ret == 0) {
+		return 0;
+	} else {
+		return -1;
+	}
+}
 
-#include <ws2tcpip.h>
-#include <stdint.h>
-#include <lua.h>
-#include <lauxlib.h>
+int
+fcntl(SOCKET fd, int cmd, int value) {
+	unsigned long on = 1;
+	return ioctlsocket(fd, FIONBIO, &on);
+}
 
-#define SO_NOSIGPIPE 0		// ignore it, don't support
+#define NS_INT16SZ   2
+#define NS_IN6ADDRSZ  16
 
-#ifndef WIN_COMPAT_IMPL
+static const char *
+inet_ntop4(const unsigned char *src, char *dst, size_t size) {
+	char tmp[sizeof "255.255.255.255"];
+	size_t len = snprintf(tmp, sizeof(tmp), "%u.%u.%u.%u", src[0], src[1], src[2], src[3]);
+	if (len >= size) {
+		return NULL;
+	}
+	memcpy(dst, tmp, len + 1);
 
-#define getsockopt win_getsockopt
-#define setsockopt win_setsockopt
-#define close closesocket
-#ifdef errno
-#undef errno
-#endif
-#define errno WSAGetLastError()
+	return dst;
+}
 
-#define EAGAIN WSATRY_AGAIN
-#define EWOULDBLOCK WSAEWOULDBLOCK
-// In windows, connect returns WSAEWOULDBLOCK rather than WSAEINPROGRESS
-#define EINPROGRESS WSAEWOULDBLOCK
+static const char *
+inet_ntop6(const unsigned char *src, char *dst, size_t size) {
+	char tmp[sizeof "ffff:ffff:ffff:ffff:ffff:ffff:255.255.255.255"], *tp;
+	struct { int base, len; } best, cur;
+	unsigned int words[NS_IN6ADDRSZ / NS_INT16SZ];
+	int i, inc;
 
-#endif
+	memset(words, '\0', sizeof(words));
+	for (i = 0; i < NS_IN6ADDRSZ; i++) {
+		words[i / 2] |= (src[i] << ((1 - (i % 2)) << 3));
+	}
+	best.base = -1;
+	best.len = 0;
+	cur.base = -1;
+	cur.len = 0;
+	for (i = 0; i < (NS_IN6ADDRSZ / NS_INT16SZ); i++) {
+		if (words[i] == 0) {
+			if (cur.base == -1) {
+				cur.base = i, cur.len = 1;
+			} else {
+				cur.len++;
+			}
+		} else if (cur.base != -1) {
+			if (best.base == -1 || cur.len > best.len) {
+				best = cur;
+			}
+			cur.base = -1;
+		}
+	}
+	if (cur.base != -1) {
+		if (best.base == -1 || cur.len > best.len)
+			best = cur;
+	}
+	if (best.base != -1 && best.len < 2)
+		best.base = -1;
 
-int win_getsockopt(SOCKET sockfd, int level, int optname, void *optval, socklen_t *optlen);
-int win_setsockopt(SOCKET sockfd, int level, int optname, const void *optval, socklen_t optlen);
+	tp = tmp;
+	for (i = 0; i < (NS_IN6ADDRSZ / NS_INT16SZ); i++) {
+		if (best.base != -1 && i >= best.base &&
+			i < (best.base + best.len)) {
+				if (i == best.base)
+					*tp++ = ':';
+			continue;
+		}
+		if (i != 0)
+			*tp++ = ':';
+		if (i == 6 && best.base == 0 &&
+			(best.len == 6 || (best.len == 5 && words[5] == 0xffff))) {
+			if (!inet_ntop4(src+12, tp, sizeof tmp - (tp - tmp)))
+				return NULL;
+			tp += strlen(tp);
+			break;
+		}
+		inc = snprintf(tp, 5, "%x", words[i]);
+		tp += inc;
+	}
+	if (best.base != -1 && (best.base + best.len) == (NS_IN6ADDRSZ / NS_INT16SZ))
+		*tp++ = ':';
+	*tp++ = '\0';
 
-// only support fcntl(fd, F_SETFL, O_NONBLOCK)
-#define F_SETFL 0
-#define O_NONBLOCK 0
-int fcntl(SOCKET fd, int cmd, int value);
-const char *inet_ntop(int af, const void *src, char *dst, socklen_t size);
+	if ((size_t)(tp - tmp) > size) {
+		return NULL;
+	}
+	memcpy(dst, tmp, tp - tmp);
+	return dst;
+}
 
-typedef u_short sa_family_t;
+const char *
+inet_ntop(int af, const void *src, char *dst,  socklen_t size) {
+	switch (af) {
+	case AF_INET:
+		return inet_ntop4((const unsigned char*)src, dst, size);
+	case AF_INET6:
+		return inet_ntop6((const unsigned char*)src, dst, size);
+	default:
+		return NULL;
+	}
+}
 
-// Windows doesn't support AF_UNIX, this structure is only for avoiding compile error
-#define UNIX_PATH_MAX    108
+void
+init_socketlib(lua_State *L) {
+	static int init = 0;
+	if (init)
+		return;
+	WSADATA wsaData;
+	int result = WSAStartup(MAKEWORD(2,2), &wsaData);
+	if (result != 0) {
+		luaL_error(L, "WSAStartup failed: %d\n", result);
+	}
+	init = 1;
+}
 
-struct sockaddr_un {
-   sa_family_t sun_family;	/* AF_UNIX */
-   char sun_path[UNIX_PATH_MAX];	/* pathname */
-};
+int
+wsa_errno() {
+	int errcode = WSAGetLastError();
+	switch (errcode) {
+	//  10009 - File handle is not valid.
+	case WSAEBADF:
+		return EBADF;
+	//  10013 - Permission denied.
+	case WSAEACCES:
+		return EACCES;
+	//  10014 - Bad address.
+	case WSAEFAULT:
+		return EFAULT;
+	//  10022 - Invalid argument.
+	case WSAEINVAL:
+		return EINVAL;
+	//  10024 - Too many open files.
+	case WSAEMFILE:
+		return EMFILE;
+	//  10035 - A non-blocking socket operation could not be completed immediately.
+	case WSAEWOULDBLOCK:
+		return EWOULDBLOCK;
+	//  10036 - Operation now in progress.
+	case WSAEINPROGRESS:
+		return EAGAIN;
+	//  10040 - Message too long.
+	case WSAEMSGSIZE:
+		return EMSGSIZE;
+	//  10043 - Protocol not supported.
+	case WSAEPROTONOSUPPORT:
+		return EPROTONOSUPPORT;
+	//  10047 - Address family not supported by protocol family.
+	case WSAEAFNOSUPPORT:
+		return EAFNOSUPPORT;
+	//  10048 - Address already in use.
+	case WSAEADDRINUSE:
+		return EADDRINUSE;
+	//  10049 - Cannot assign requested address.
+	case WSAEADDRNOTAVAIL:
+		return EADDRNOTAVAIL;
+	//  10050 - Network is down.
+	case WSAENETDOWN:
+		return ENETDOWN;
+	//  10051 - Network is unreachable.
+	case WSAENETUNREACH:
+		return ENETUNREACH;
+	//  10052 - Network dropped connection on reset.
+	case WSAENETRESET:
+		return ENETRESET;
+	//  10053 - Software caused connection abort.
+	case WSAECONNABORTED:
+		return ECONNABORTED;
+	//  10054 - Connection reset by peer.
+	case WSAECONNRESET:
+		return ECONNRESET;
+	//  10055 - No buffer space available.
+	case WSAENOBUFS:
+		return ENOBUFS;
+	//  10057 - Socket is not connected.
+	case WSAENOTCONN:
+		return ENOTCONN;
+	//  10060 - Connection timed out.
+	case WSAETIMEDOUT:
+		return ETIMEDOUT;
+	//  10061 - Connection refused.
+	case WSAECONNREFUSED:
+		return ECONNREFUSED;
+	//  10065 - No route to host.
+	case WSAEHOSTUNREACH:
+		return EHOSTUNREACH;
+	default:
+		//  Not reachable
+		return 0;
+	}
+}
 
-void init_socketlib(lua_State *L);
+int
+win_select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, struct timeval * const timeout) {
+	if (writefds == NULL) {
+		return select(nfds, readfds, NULL, NULL, timeout);
+	}
+	fd_set exfd;
+	FD_ZERO(&exfd);
 
-#endif
+	// copy fd_set
+	int i;
+	for (i=0;i<writefds->fd_count;i++) {
+		FD_SET(writefds->fd_array[i], &exfd);
+	}
+	int r = select(nfds, readfds, writefds, &exfd, timeout);
+	if (r > 0) {
+		for (i=0;i<exfd.fd_count;i++) {
+			FD_SET(exfd.fd_array[i], writefds);
+		}
+	}
+	return r;
+}
